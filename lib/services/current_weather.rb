1bb29a689bcd7a4649d1a3cc521f9c56d06543d3
Tech-A45 <tech-a45@Tech-A45xs-MacBook-Air.local>
2019-01-31 11:38:22 +0000

    able to get the current weather from the API using both name and id and print into the terminal for a single city

--- lib/services/current_weather.rb
+++ lib/services/current_weather.rb
@@ -7,11 +7,19 @@ class CurrentWeather
 
   base_uri 'http://api.openweathermap.org'
 
-  def current_weather_request(city_name, api_key)
-    @single_weather_request = JSON.parse(self.class.get("/data/2.5/weather?id=#{city_name}&APPID=#{api_key}").body)
-
+  def current_weather_request_by_name(city_name, api_key)
+    @single_weather_request_by_name = JSON.parse(self.class.get("/data/2.5/weather?q=#{city_name}&APPID=#{api_key}").body)
   end
+
+  def current_weather_request_by_id(id, api_key)
+    @single_weather_request_by_name = JSON.parse(self.class.get("/data/2.5/weather?id=#{id}&APPID=#{api_key}").body)
+  end
+
+
 end
 
 test = CurrentWeather.new
-p test.current_weather_request('2648110','5f665a87a58fb70f8412f3a160995328')
+p test.current_weather_request_by_name('Kathmandu','5f665a87a58fb70f8412f3a160995328')
+
+test = CurrentWeather.new
+puts test.current_weather_request_by_id('2648110', '5f665a87a58fb70f8412f3a160995328')
